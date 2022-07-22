8b3c4afd3f618444c74b8324b0942c5d7ac9046e
Aaron Ayub <55106255+AaronAyub@users.noreply.github.com>
2022-07-21 23:37:27 -0400

    Initial commit. Adding strlib functions

---
+++ README.md
@@ -0,0 +1,9 @@
+# string-tension
+A program to calculate the tensions of guitar or bass strings.
+
+This project is currently under development.
+
+### Usage
+
+### License
+This script is available under the [MIT license](https://github.com/AaronAyub/string-tension/blob/main/LICENSE).
---
+++ strlib.cpp
@@ -0,0 +1,15 @@
+/* Library for calculating dimensions of a guitar/bass string, and related note properties. */
+#include <cmath>
+
+namespace strlib {
+    /* Calculate a string's tension. Returns the tension in pounds. */
+    double calcTension(double unitWeight, double scaleLength, double frequency) {
+        return unitWeight * std::pow((2 * scaleLength * frequency),2) / 386.4;
+    }
+
+    /* Returns the frequency associated with a provided MIDI note number*/
+    double noteToFrequency(int note) {
+        double diff = static_cast<double>(note - 69); // Difference of the note from A4
+        return 440 * std::pow(2,(diff / 12));
+    }
+}
