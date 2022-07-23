1e19968b41d0cb0b199edb07b1daa4320247a034
Aaron Ayub <55106255+AaronAyub@users.noreply.github.com>
2022-07-23 17:39:49 -0400

    Add stringweight functionality to strlib header

--- src/strlib/strlib.h
+++ src/strlib/strlib.h
@@ -1,5 +1,6 @@
 /* Library for calculating dimensions of a guitar/bass string, and related note properties. */
 #include <cmath>
+#include "stringweight.h"
 
 namespace strlib {
     double calcTension(double unitWeight, double scaleLength, double frequency);
